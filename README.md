e674ade073f5c760de9acb6ad3b25e62b5b01133
Ali Nuraldin <alirunwwc@gmail.com>
2023-01-31 21:58:23 +0100

    Production deployment is ready

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Dormint Pillows Genesis
 
+## Traits Contract
+
+Deployed at [0x349F91692c9c5441fD94020b6984D557cbbEd4c3](https://polygonscan.com/address/0x349f91692c9c5441fd94020b6984d557cbbed4c3)
+
 ## NFT TODO
 - [x] Whitelist functionality
 - [x] VRF request + response (compatible with upgradeability)
@@ -12,10 +16,3 @@
 - [ ] Testnet deployment
 - [ ] Mainnet deployment
 
-## Traits Contract TODO
-- [x] VRF request + response (compatible with upgradeability)
-- [x] Traits reveal
-- [x] Tests
-- [x] Deployment script
-- [x] Testnet deployment
-- [ ] Mainnet deployment
---
+++ deploy/002_deploy_traits_instance_proxy_prod.ts
@@ -0,0 +1,38 @@
+
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  if (network.name !== 'polygon') {
+    return
+  }
+
+  const LINK_ADDRESS = "0xb0897686c545045aFc77CF20eC7A532E3120E0F1";
+  const VRF_WRAPPER_ADDRESS = "0x4e42f0adEB69203ef7AaA4B7c414e5b1331c14dc";
+
+  const deployResult = await deploy("DormintPillowsTraits", {
+    from: deployer,
+    proxy: {
+      proxyContract: "OpenZeppelinTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [LINK_ADDRESS, VRF_WRAPPER_ADDRESS],
+        },
+      },
+    },
+    log: true,
+  });
+
+  console.log(`✓ DormintPillowsTraits Deployed at ${deployResult.address}`);
+
+  return true;
+};
+export default func;
+func.id = "002";
+func.tags = ["DormintPillowsTraits"];
